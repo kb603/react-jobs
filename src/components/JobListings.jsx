be171acd76c4aec8423252e31b5771fd664c1113
kb603 <kojoaddobaffour@gmail.com>
2024-12-06 17:29:38 +0000

    updates

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,21 +1,44 @@
 /* eslint-disable react/prop-types */
-
-import jobs from "../jobs.json";
+import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 export default function JobListings({ isHome = false }) {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? "api/jobs?_limit=3" : "api/jobs";
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching jobs data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  });
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl m-auto lg:container">
         <h2 className="mb-6 text-center text-3xl font-bold text-indigo-500">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-          {jobListings.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
-        </div>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+export default function Spinner({ loading }) {
+  return (
+    <ClipLoader
+      color="#4338ca"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
+  );
+}
