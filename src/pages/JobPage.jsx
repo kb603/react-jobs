66216b9b36b56d4ca3fea34f43c1b7ac2f24b70f
kb603 <kojoaddobaffour@gmail.com>
2024-12-07 19:40:03 +0000

    updates

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,8 +1,103 @@
-import { useParams } from "react-router-dom";
-import Spinner from "../components/Spinner";
+import { useParams, useLoaderData } from "react-router-dom";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-export default function JobPage() {
+const JobPage = () => {
   const { id } = useParams();
+  const job = useLoaderData();
 
-  return loading ? <Spinner /> : <h1>{job.title}</h1>;
-}
+  return (
+    <>
+      <section>
+        <div className="container m-auto px-6 py-6">
+          <Link
+            to="/jobs"
+            className="flex items-center text-indigo-500 hover:text-indigo-600"
+          >
+            <FaArrowLeft className="mr-2" />
+            Back to Job Listings
+          </Link>
+        </div>
+      </section>
+
+      <section className="bg-indigo-50">
+        <div className="container m-auto px-6 py-10">
+          <div className="grid w-full grid-cols-1 gap-6 md:grid-cols-70/30">
+            <main>
+              <div className="rounded-lg bg-white p-6 text-center shadow-md md:text-left">
+                <div className="mb-4 text-gray-500">{job.type}</div>
+                <h1 className="mb-4 text-3xl font-bold">{job.title}</h1>
+                <div className="mb-4 flex justify-center align-middle text-gray-500 md:justify-start">
+                  <FaMapMarker className="mr-2 text-orange-700" />
+                  <p className="text-orange-700">{job.location}</p>
+                </div>
+              </div>
+
+              <div className="mt-6 rounded-lg bg-white p-6 shadow-md">
+                <h3 className="mb-6 text-lg font-bold text-indigo-800">
+                  Job Description
+                </h3>
+
+                <p className="mb-4">{job.description}</p>
+
+                <h3 className="mb-2 text-lg font-bold text-indigo-800">
+                  Salary
+                </h3>
+
+                <p className="mb-4">{job.salary}</p>
+              </div>
+            </main>
+
+            {/* <!-- Sidebar --> */}
+            <aside>
+              {/* <!-- Company Info --> */}
+              <div className="rounded-lg bg-white p-6 shadow-md">
+                <h3 className="mb-6 text-xl font-bold">Company Info</h3>
+
+                <h2 className="text-2xl">{job.company.name}</h2>
+
+                <p className="my-2">{job.company.description}</p>
+
+                <hr className="my-4" />
+
+                <h3 className="text-xl">Contact Email:</h3>
+
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactEmail}
+                </p>
+
+                <h3 className="text-xl">Contact Phone:</h3>
+
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
+              </div>
+
+              {/* <!-- Manage --> */}
+              <div className="mt-6 rounded-lg bg-white p-6 shadow-md">
+                <h3 className="mb-6 text-xl font-bold">Manage Job</h3>
+                <Link
+                  to={`/jobs/edit/${job.id}`}
+                  className="focus:shadow-outline mt-4 block w-full rounded-full bg-indigo-500 px-4 py-2 text-center font-bold text-white hover:bg-indigo-600 focus:outline-none"
+                >
+                  Edit Job
+                </Link>
+                <button className="focus:shadow-outline mt-4 block w-full rounded-full bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-600 focus:outline-none">
+                  Delete Job
+                </button>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
